95899294f82a339314f34131da1e31caca4c9bd9
ogullcankara <ogullcankara@gmail.com>
2022-09-26 12:35:01 +0300

    Initial Commit

---
+++ ArtBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBook
+//
+//  Created by Ogulcan Kara on 26.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
