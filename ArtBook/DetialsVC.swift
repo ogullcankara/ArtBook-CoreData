82418be5695de8adbca1b35b9037c9f78e6b35a6
ogullcankara <ogullcankara@gmail.com>
2022-09-27 01:37:44 +0300

    First commit

---
+++ ArtBook/DetialsVC.swift
@@ -0,0 +1,63 @@
+//
+//  DetialsVC.swift
+//  ArtBook
+//
+//  Created by Ogulcan Kara on 26.09.2022.
+//
+
+import UIKit
+
+class DetialsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var artistText: UITextField!
+    
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        //Recognizers
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTabRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTabRecognizer)
+        
+    }
+    
+    @objc func selectImage(){
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        print("Test")
+        
+    }
+    
+}
--- ArtBook/ViewController.swift
+++ ArtBook/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+    }
+    
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toDetialsVC", sender: nil)
     }
 
 
