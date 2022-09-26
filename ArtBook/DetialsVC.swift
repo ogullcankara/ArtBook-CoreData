a19b0b70a78777bb7a645eaee051e84e56e73c8d
ogullcankara <ogullcankara@gmail.com>
2022-09-27 02:41:24 +0300

    Added fetch data function

--- ArtBook/DetialsVC.swift
+++ ArtBook/DetialsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetialsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -56,8 +57,31 @@ class DetialsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
 
     @IBAction func saveButtonClicked(_ sender: Any) {
         
-        print("Test")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attributes
+        newPainting.setValue(nameText.text, forKey: "name")
+        newPainting.setValue(artistText.text, forKey: "artist")
+        
+        if let year = Int(yearText.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        do{
+            try context.save()
+            print("Success")
+        }catch{
+            print("Error")
+        }
     }
+    
     
 }
